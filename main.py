5b68586083e1e44ee07a7067eec16486f9faf590
SatoshiLC500 <116571452+SatoshiLC500@users.noreply.github.com>
2022-11-22 01:03:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tetelek
 
-# tetelek.atlag()
-# tetelek.osszefuzes(3, "Keresztnév: ")
-# tetelek.megszamlal(0, 100, 2)
+tetelek.atlag()
+tetelek.osszefuzes(3, "Keresztnév: ")
+tetelek.megszamlal(0, 100, 2)
 tetelek.veletlen(2, 8, 7)
